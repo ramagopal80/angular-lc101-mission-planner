36b6cbf3073ade8c4bfc6c9395ac6f1b0e9e72a2
Rama <ramagopal80@gmail.com>
2020-11-02 21:25:34 -0600

    commited lesson 1 studio

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CrewComponent } from './crew/crew.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentComponent } from './experiment/experiment.component';
+// import { Crew } from './crew./crew..component';
+// import { CrewComponent } from './crew/crew.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    CrewComponent,
+    EquipmentComponent,
+    ExperimentComponent,
+      ],
   imports: [
     BrowserModule
   ],
